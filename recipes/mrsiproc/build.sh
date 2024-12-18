8acc2b84735eced93ae934ab19abf60da4029033
Korbinian Eckstein <korbinian90@gmail.com>
2024-12-18 08:06:29 +0000

    add hMRI toolbox

--- recipes/mrsiproc/build.sh
+++ recipes/mrsiproc/build.sh
@@ -4,8 +4,9 @@ set -e
 # this template file builds datalad and is then used as a docker base image for layer caching + it contains examples for various things like github install, curl, ...
 export toolName='mrsiproc'
 export toolVersion='0.2.0' # the version number cannot contain a "-" - try to use x.x.x notation always
-export matlabVersion='2021b' # this has to match the version on which the matlab scripts were compiled
-export matlabUpdateVersion='6'
+export matlabMCRVersion='2023a' # matlab MCR has to match the version on which the matlab scripts were compiled
+export matlabUpdateVersion='7'
+export hMRIVersion='0.6.1' # this requires matlab MCR 2023a (Update 1)
 export mincVersion='1.9.15'
 export lcmodelVersion='6.3'
 export hdbetVersion='1.0' # note the hdbet doesn't really have a version
@@ -13,7 +14,7 @@ export fslVersion='6.0.7.1' # build on ubuntu 20.04
 export minicondaVersion='latest'
 export dcm2niixVersion='003f0d19f1e57b0129c9dcf3e653f51ca3559028' # copied from qsmxt
 export freesurferVersion='7.4.1'
-export mrsi_pipeline_version='7090915b7d99cb503e8b0685d5e0c78dc242caf3'
+export mrsi_pipeline_version='86a9de2ac939f3eafc1a1a980b85513896e0abc0'
 
 # Don't forget to update version change in README.md!!!!!
 # toolName or toolVersion CANNOT contain capital letters or dashes or underscores (Docker registry does not accept this!)
@@ -97,22 +98,28 @@ neurodocker generate ${neurodocker_buildMode} \
    --env DEPLOY_PATH=/opt/lcmodel-${lcmodelVersion}/.lcmodel/bin/:/opt/lcmodel-${lcmodelVersion}/.lcmodel/ \
    --env PATH="\${PATH}:/opt/lcmodel-${lcmodelVersion}/.lcmodel/bin/:/opt/lcmodel-${lcmodelVersion}/.lcmodel/" \
    \
-   `# Add MATLAB compiler runtime (doesn't need license)` \
-   --install bc curl libncurses5 libxext6 libxmu6 libxpm-dev libxt6 unzip openjdk-8-jre dbus-x11 \
-   --run="wget https://ssd.mathworks.com/supportfiles/downloads/R${matlabVersion}/Release/${matlabUpdateVersion}/deployment_files/installer/complete/glnxa64/MATLAB_Runtime_R${matlabVersion}_Update_${matlabUpdateVersion}_glnxa64.zip && \
-          unzip MATLAB_Runtime_R${matlabVersion}_Update_${matlabUpdateVersion}_glnxa64.zip -d mcrtmp && \
-          mcrtmp/install -mode silent -destinationFolder /opt/MATLAB_Runtime_R${matlabVersion} -agreeToLicense yes && \
-          rm -rf mcrtmp && \
-          rm -f MATLAB_Runtime_R${matlabVersion}_Update_${matlabUpdateVersion}_glnxa64.zip" \
-   \
-   --env LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:/opt/MATLAB_Runtime_R${matlabVersion}/R${matlabVersion}/runtime/glnxa64:/opt/MATLAB_Runtime_R${matlabVersion}/R${matlabVersion}/bin/glnxa64:/opt/MATLAB_Runtime_R${matlabVersion}/R${matlabVersion}/sys/os/glnxa64:/opt/MATLAB_Runtime_R${matlabVersion}/R${matlabVersion}/extern/bin/glnxa64" \
-   \
    --workdir /opt `# Add freesurfer` \
-   --install language-pack-en gettext xterm x11-apps csh tcsh xorg xorg-dev xserver-xorg-video-intel libwayland-cursor0 \
+   --install language-pack-en libncurses5 gettext xterm x11-apps csh tcsh xorg xorg-dev xserver-xorg-video-intel libwayland-cursor0 \
    --run="wget --quiet https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/${freesurferVersion}/freesurfer_ubuntu20-${freesurferVersion}_amd64.deb \
             && dpkg -i freesurfer_ubuntu20-${freesurferVersion}_amd64.deb \
             && rm -f freesurfer_ubuntu20-${freesurferVersion}_amd64.deb" \
    --env PATH="\${PATH}:/usr/local/freesurfer/${freesurferVersion}-1" \
+   \
+   `# Add MATLAB compiler runtime (doesn't need license)` \
+   --install bc curl libncurses5 libxext6 libxmu6 libxpm-dev libxt6 unzip openjdk-8-jre dbus-x11 \
+   --run="wget https://ssd.mathworks.com/supportfiles/downloads/R${matlabMCRVersion}/Release/${matlabUpdateVersion}/deployment_files/installer/complete/glnxa64/MATLAB_Runtime_R${matlabMCRVersion}_Update_${matlabUpdateVersion}_glnxa64.zip && \
+          unzip MATLAB_Runtime_R${matlabMCRVersion}_Update_${matlabUpdateVersion}_glnxa64.zip -d mcrtmp && \
+          mcrtmp/install -mode silent -destinationFolder /opt/MATLAB_Runtime_R${matlabMCRVersion} -agreeToLicense yes && \
+          rm -rf mcrtmp && \
+          rm -f MATLAB_Runtime_R${matlabMCRVersion}_Update_${matlabUpdateVersion}_glnxa64.zip" \
+   \
+   --env LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:/opt/MATLAB_Runtime_R${matlabMCRVersion}/R${matlabMCRVersion}/runtime/glnxa64:/opt/MATLAB_Runtime_R${matlabMCRVersion}/R${matlabMCRVersion}/bin/glnxa64:/opt/MATLAB_Runtime_R${matlabMCRVersion}/R${matlabMCRVersion}/sys/os/glnxa64:/opt/MATLAB_Runtime_R${matlabMCRVersion}/R${matlabMCRVersion}/extern/bin/glnxa64" \
+   \
+   --workdir /opt `# Add hMRI toolbox` \
+   --run="wget -q https://github.com/hMRI-group/hMRI-toolbox/releases/download/v${hMRIVersion}/standalone-hMRItoolboxv${hMRIVersion}.zip \
+            && unzip standalone-hMRItoolboxv${hMRIVersion}.zip \
+            && rm -f standalone-hMRItoolboxv${hMRIVersion}.zip" \
+   --env PATH="\${PATH}:/opt/standalone-hMRItoolboxv0.6.1" \
    \
    --workdir /opt `# Add MRSI pipeline scripts` \
    --install parallel \
@@ -124,7 +131,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod a+x /opt/mrsi_pipeline_neurodesk/update_mrsi.sh" \
    --env PATH="/neurodesktop-storage/mrsi_pipeline_neurodesk/Part1:/neurodesktop-storage/mrsi_pipeline_neurodesk/Part2:/opt/mrsi_pipeline_neurodesk:\${PATH}" \
    --env DEPLOY_PATH="/opt/mrsi_pipeline_neurodesk/Part1:/opt/mrsi_pipeline_neurodesk/Part2" \
-   --env DEPLOY_BINS="julia:python:bc:dcm2niix:nii2mnc:dcm2mnc:rawtominc:mincresample:mincmath:minctoraw:mnc2nii:bet:hd-bet:mri_synthseg:lcmodel:parallel:fsl:fslmaths:Part1_ProcessMRSI.sh:Part2_EvaluateMRSI.sh:update_mrsi.sh:fid_1.300000ms.basis:LCModel_Control_Template.m:LCModel_Control_Volunteers_WatRef.m" \
+   --env DEPLOY_BINS="matlab:mex:julia:python:bc:dcm2niix:nii2mnc:dcm2mnc:rawtominc:mincresample:mincmath:minctoraw:mnc2nii:bet:hd-bet:mri_synthseg:lcmodel:parallel:fsl:fslmaths:Part1_ProcessMRSI.sh:Part2_EvaluateMRSI.sh:update_mrsi.sh:fid_1.300000ms.basis:LCModel_Control_Template.m:LCModel_Control_Volunteers_WatRef.m" \
    \
    --copy README.md /README.md                          `# include README file in container` \
    \
